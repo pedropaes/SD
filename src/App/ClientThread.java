814fe1d622ffeaa76ee132af5c69d7e1a09ab2f8
Jose Alves <jalves9200@gmail.com>
2018-12-26 23:35:33 +0000

    Menus mais legiveis

--- src/App/ClientThread.java
+++ src/App/ClientThread.java
@@ -1,9 +1,6 @@
 package App;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -201,6 +198,7 @@ class ClientThread extends Thread{
             case "2":   login();
                         break;
             case "3":   running = false;
+                        System.exit(0);
                         break;
             default:    System.out.println("\nOpção Inválida");
                         break;
